fb79c28097b6ce9c53278cfcf27c42b94437a947
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-01-17 08:22:25 -0300

    chore: improve footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,110 +1,55 @@
-import Link from '@docusaurus/Link';
 import { memo, type ReactNode } from 'react';
-import { Github, Instagram, Linkedin } from 'lucide-react';
 import { SafeLink } from '@site/src/components/SafeLink';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { socials } from '@site/src/helpers/get-contents';
+
+const footers: Record<string, ReactNode> = {
+  'pt-BR': (
+    <p>
+      Direitos Reservados © {new Date().getFullYear()}{' '}
+      <SafeLink to='https://github.com/wellwelwel'>
+        <strong className='feat'>Weslley Araújo</strong>
+      </SafeLink>{' '}
+      sob a licença{' '}
+      <SafeLink to='https://github.com/wellwelwel/weslley.io/blob/main/LICENSE'>
+        <strong className='underline'>
+          GNU Affero General Public License v3.0
+        </strong>
+      </SafeLink>
+      .
+    </p>
+  ),
+  en: (
+    <p>
+      Copyright © {new Date().getFullYear()}{' '}
+      <SafeLink to='https://github.com/wellwelwel'>
+        <strong className='feat'>Weslley Araújo</strong>
+      </SafeLink>{' '}
+      under the{' '}
+      <SafeLink to='https://github.com/wellwelwel/weslley.io/blob/main/LICENSE'>
+        <strong className='underline'>
+          GNU Affero General Public License v3.0
+        </strong>
+      </SafeLink>{' '}
+      license.
+    </p>
+  ),
+};
 
 const Footer = (): ReactNode => {
   const { i18n } = useDocusaurusContext();
   const { currentLocale } = i18n;
-  const isPtBr = currentLocale === 'pt-BR';
+  const Socials = socials(currentLocale);
 
-  return isPtBr ? (
+  return (
     <footer>
       <aside className='left'>
-        <img loading='lazy' src='/img/osi.svg' alt='' />
-        <section>
-          <p>
-            <strong>
-              <Link to='/'>
-                <strong>weslley.io</strong>
-              </Link>
-            </strong>{' '}
-            é um site que serve como um template{' '}
-            <SafeLink to='https://github.com/wellwelwel/weslley.io'>
-              <strong>open-source</strong>
-            </SafeLink>{' '}
-            sob a licença{' '}
-            <SafeLink to='https://github.com/wellwelwel/weslley.io/blob/main/LICENSE'>
-              <strong className='underline'>AGPL-3.0</strong> (GNU Affero
-              General Public License)
-            </SafeLink>
-            .
-          </p>
-          <p>
-            Direitos Reservados © 2024-present{' '}
-            <SafeLink to='https://github.com/wellwelwel'>
-              <strong className='feat'>Weslley Araújo</strong>
-            </SafeLink>
-            .
-          </p>
-        </section>
+        <section>{footers[currentLocale]}</section>
       </aside>
       <aside className='right'>
-        <SafeLink
-          to='https://www.linkedin.com/in/wellwelwel/'
-          className='underline'
-        >
-          <Linkedin />
-        </SafeLink>
-        <SafeLink to='https://github.com/wellwelwel' className='underline'>
-          <Github />
-        </SafeLink>
-        <SafeLink
-          to='https://www.instagram.com/wellwelwel/'
-          className='underline'
-        >
-          <Instagram />
-        </SafeLink>
-      </aside>
-    </footer>
-  ) : (
-    <footer>
-      <aside className='left'>
-        <img loading='lazy' src='/img/osi.svg' alt='' />
-        <section>
-          <p>
-            <strong>
-              <Link to='/'>
-                <strong>weslley.io</strong>
-              </Link>
-            </strong>{' '}
-            is a website which serves as an{' '}
-            <SafeLink to='https://github.com/wellwelwel/weslley.io'>
-              <strong>open-source</strong>
-            </SafeLink>{' '}
-            template under the{' '}
-            <SafeLink to='https://github.com/wellwelwel/weslley.io/blob/main/LICENSE'>
-              <strong className='underline'>AGPL-3.0</strong> (GNU Affero
-              General Public License)
-            </SafeLink>{' '}
-            license .
-          </p>
-          <p>
-            Copyright © 2024-present{' '}
-            <SafeLink to='https://github.com/wellwelwel'>
-              <strong className='feat'>Weslley Araújo</strong>
-            </SafeLink>
-            .
-          </p>
-        </section>
-      </aside>
-      <aside className='right'>
-        <SafeLink
-          to='https://www.linkedin.com/in/wellwelwel/'
-          className='underline'
-        >
-          <Linkedin />
-        </SafeLink>
-        <SafeLink to='https://github.com/wellwelwel' className='underline'>
-          <Github />
-        </SafeLink>
-        <SafeLink
-          to='https://www.instagram.com/wellwelwel/'
-          className='underline'
-        >
-          <Instagram />
-        </SafeLink>
+        {Socials.map((Social, i) => (
+          <Social key={`card:${i}`} />
+        ))}
       </aside>
     </footer>
   );
