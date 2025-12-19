57b5502aa46cf7f3c7e2c59cbf7ddf41cdca648f
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-12-19 08:18:35 -0300

    chore: improve descriptions

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -20,7 +20,7 @@ export default (): ReactNode => {
   return (
     <Layout
       title={title}
-      description='Lista com a maioria dos projetos que mantenho e co-mantenho através da iniciativa open source.'
+      description='Lista com a maioria dos projetos que mantenho através da iniciativa open source.'
     >
       <div id='projects'>
         <main>
